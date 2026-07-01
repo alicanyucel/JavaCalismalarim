87422b6c46711dc219a4dea0e071d66f162752cf
Ali Can Yücel <yucelalican@hotmail.com>
2026-07-01 16:51:28 +0300

    ok

--- Merhaba/main.java
+++ Merhaba/main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Merhaba Java Core!");
-    }
+   public static void main(String[] args) {
+      System.out.println("Ali Can Yücel"); 
+   }
 }
